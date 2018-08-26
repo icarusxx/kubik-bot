d34d35008938c28f55cd1c99d63ed4388f3981a7
icarusxx <42697793+icarusxx@users.noreply.github.com>
2018-08-26 22:52:55 +0800

    Update sdata-modules.php

--- sdata-modules.php
+++ sdata-modules.php
@@ -43,7 +43,78 @@ class Modules
 		    	curl_setopt($ch[$i], CURLOPT_HTTPHEADER, $custom[$i]['header']);
 		    }
 		    if($custom[$i]['post']){
+		    	if(is_array($custom[$i]['p<?php
+error_reporting(0);
+/**
+ * @Author: Eka Syahwan
+ * @Date:   2017-11-06 22:54:36
+ * @Last Modified by:   Eka Syahwan
+ * @Last Modified time: 2017-12-11 17:12:21
+ */
+class Modules
+{
+	public function sdata($url = null , $custom = null){
+		mkdir('cookies'); // pleas don't remove
+		$ch 	 	= array();
+		$mh 		= curl_multi_init();
+		$total 		= count($url);
+		$allrespons = array();
+		for ($i = 0; $i < $total; $i++) {
+			if($url[$i]['cookies']){
+				$cookies		= $url[$i]['cookies'];
+			}else{
+				$cookies 		= 'cookies/shc-'.md5($this->cookies())."-".time().'.txt'; 
+			}
+			$ch[$i] 			= curl_init();
+			$threads[$ch[$i]] 	= array(
+				'proses_id' => $i,
+				'url' 		=> $url[$i]['url'],
+				'cookies' 	=> $cookies, 
+				'note' 		=> $url[$i]['note'],
+			);
+		    curl_setopt($ch[$i], CURLOPT_URL, $url[$i]['url']);
+			if($custom[$i]['gzip']){
+				curl_setopt($ch[$i], CURLOPT_ENCODING , "gzip");
+			}
+		    curl_setopt($ch[$i], CURLOPT_HEADER, false);
+		    curl_setopt($ch[$i], CURLOPT_COOKIEJAR,  $cookies);
+      		curl_setopt($ch[$i], CURLOPT_COOKIEFILE, $cookies);
+		    if($custom[$i]['rto']){
+		    	curl_setopt($ch[$i], CURLOPT_TIMEOUT, $custom[$i]['rto']);
+		    }else{
+		    	curl_setopt($ch[$i], CURLOPT_TIMEOUT, 60);
+		    }
+		    if($custom[$i]['header']){
+		    	curl_setopt($ch[$i], CURLOPT_HTTPHEADER, $custom[$i]['header']);
+		    }
+		    if($custom[$i]['post']){
 		    	if(is_array($custom[$i]['post'])){
+		    		$query = http_build_query($custom[$i]['post']);
+		    	}else{
+		    		$query = $custom[$i]['post'];
+		    	}
+		    	curl_setopt($ch[$i], CURLOPT_POST, true);
+		    	curl_setopt($ch[$i], CURLOPT_POSTFIELDS, $query);
+		    }
+		    if($custom[$i]['proxy']){
+		    	curl_setopt($ch[$i], CURLOPT_PROXY, 	$custom[$i]['proxy']['ip']);
+		    	curl_setopt($ch[$i], CURLOPT_PROXYPORT, $custom[$i]['proxy']['port']);
+		    	if( $custom[$i]['proxy']['type'] ){
+		    		curl_setopt($ch[$i], CURLOPT_PROXYTYPE, $custom[$i]['proxy']['type']);
+		    	}
+		    }
+		    curl_setopt($ch[$i], CURLOPT_VERBOSE, false);
+		    curl_setopt($ch[$i], CURLOPT_CONNECTTIMEOUT , 0);
+		    curl_setopt($ch[$i], CURLOPT_RETURNTRANSFER, true);
+		    curl_setopt($ch[$i], CURLOPT_FOLLOWLOCATION, true);
+		    curl_setopt($ch[$i], CURLOPT_SSL_VERIFYPEER, false);
+		    curl_setopt($ch[$i], CURLOPT_SSL_VERIFYHOST, false); 
+        	if($custom[$i]['uagent']){
+		    	curl_setopt($ch[$i], CURLOPT_USERAGENT, $custom[$i]['uagent']);
+		    }else{
+				curl_setopt($ch[$i], CURLOPT_USERAGENT, "Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) CriOS/42.0.2311.47 Mobile/12F70 Safari/600.1.4");
+		    }
+	    	curl_multi_add_handle($mh, $ch[$i]);ost'])){
 		    		$query = http_build_query($custom[$i]['post']);
 		    	}else{
 		    		$query = $custom[$i]['post'];
